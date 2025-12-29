b265b7bb0be3bbc7317e55f2048a3e608a7626da
Simon Nilsson <s.i.m.o.n89.sn@gmail.com>
2025-12-29 13:14:11 +0100

    Add project files.

---
+++ CManager.Application/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using CManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Application.Services;
+
+public interface ICustomerService
+{
+    bool CreateCustomer(string firstName, string lastName, string email, string phoneNumber, string streetAddress, string postalCode, string city);
+
+    IEnumerable<CustomerModel> GetAllCustomers(out bool hasError);
+}
---
+++ CManager.Infrastructure/Repos/ICustomerRepo.cs
@@ -0,0 +1,12 @@
+﻿using CManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Infrastructure.Repos;
+
+public interface ICustomerRepo
+{
+    List<CustomerModel> GetAllCustomers();
+    bool SaveCustomers(List<CustomerModel> customers);
+}
