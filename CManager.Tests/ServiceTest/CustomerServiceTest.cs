d0e4dbcd91d9ce1af2d993b37648c56f87f11e4b
Simon Nilsson <s.i.m.o.n89.sn@gmail.com>
2026-01-08 15:03:56 +0100

    9th commit. Test for CustomerServiceModel on DeleteCustomerByEmail method done.

--- CManager.Tests/ServiceTest/CustomerServiceTest.cs
+++ CManager.Tests/ServiceTest/CustomerServiceTest.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CManager.Application.Services;
+using CManager.Domain.Models;
+using CManager.Infrastructure.Repos;
+using Moq;
 
-namespace CManager.Tests.ServiceTest
+namespace CManager.Tests.ServiceTests;
+
+public class CustomerServiceTest
 {
-    internal class CustomerServiceTest
+    [Fact]
+    public void DeleteCustomer_WhenCustomerDoesNotExist_ReturnsFalse()
     {
+        //arrange
+        var mockCustomerRepo = new Mock<ICustomerRepo>();
+
+        mockCustomerRepo
+            .Setup(r => r.GetAllCustomers())
+            .Returns(new List<CustomerModel>());
+
+        var service = new CustomerService(mockCustomerRepo.Object);
+         
+        //act
+        var result = service.DeleteCustomerByEmail("test@test.se");
+
+        //assert
+        Assert.False(result);
+        mockCustomerRepo.Verify(r => r.GetAllCustomers(), Times.Once);
+        mockCustomerRepo.Verify(r => r.SaveCustomers(It.IsAny<List<CustomerModel>>()), Times.Never);
     }
 }
+
